b1a6ffdad74db2ea6a4c8e3ac8d3d59ec155811b
oabuhamdeh <oabuhamdeh@brandeis.edu>
2023-06-20 13:35:09 -0400

    fixed the meoji bug

--- Quiz 2/TestScreen.js
+++ Quiz 2/TestScreen.js
@@ -9,6 +9,7 @@ import CircleButton from './components/CircleButton';
 import IconButton from './components/IconButton';
 import EmojiPicker from "./components/EmojiPicker";
 import EmojiList from './components/EmojiList';
+import EmojiSticker from './components/EmojiSticker';
 
 const PlaceholderImage = require('./background-image.png');
 
@@ -55,6 +56,7 @@ export default function App() {
           placeholderImageSource={PlaceholderImage}
           selectedImage={selectedImage}
         />
+        {pickedEmoji !== null ? <EmojiSticker imageSize={40} stickerSource={pickedEmoji} /> : null}
       </View>
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
